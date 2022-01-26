d1909c794ad117810912967de6ff7decc3ade45e
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2022-01-26 17:53:43 -0300

    pratica 3

---
+++ filter/filtraPares.js
@@ -0,0 +1,17 @@
+// function filtraPares(array) {
+//     return array.filter(callback)
+// }
+
+// function callback(item) {
+//     return item % 2 === 0;
+// }
+
+function filtraPares(array) {
+    return array.filter(function(item) {
+        return item % 2 === 0;
+    });
+}
+
+const meuArray = [1, 23, 55, 67, 30, 2, 4];
+
+console.log(filtraPares(meuArray));
