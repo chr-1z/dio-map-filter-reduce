03ee4a639e8892069d71bf44bbf56b19cbf4c069
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2022-01-26 17:43:53 -0300

    pratica 1

---
+++ map/comThis.js
@@ -0,0 +1,19 @@
+const apple = {
+    value : 2
+}
+
+const orange = {
+    value : 3
+}
+
+function mapComThis(array, thisArg) {
+    return array.map(function(item) {
+        return item * this.value;
+    }, thisArg);
+}
+
+const nums = [1, 2];
+
+console.log('this -> apple', mapComThis(nums, apple));
+
+console.log('this -> orange', mapComThis(nums, orange));
